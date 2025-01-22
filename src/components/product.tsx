136748479c6749c2d249738737f297fedb1b8e9f
venkadesh <venkadesh.sreekumar@contenstack.com>
2025-01-22 15:27:10 +0530

    updated review

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -5,11 +5,27 @@ import { Products } from '@/typescript/response';
 import React, { useEffect, useState } from 'react'
 import Icon from '@/assets/Icon.png';
 import Image from 'next/image';
+import axios from 'axios';
 
 export default function Product({ entryUrl }: { entryUrl: string }) {
 
     const [product, setProduct] = useState({} as Products);
     const [loading, setLoading] = useState(true);
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        var rating = e.target.rating.value;
+        var review = e.target.review.value;
+        var item = product.title;
+
+        axios.post('https://app.contentstack.com/automations-api/run/3c2d616d80b447808fb63321ca40ab35', {rating, review, item}).then(res => {
+            if (res.status === 200) {
+                alert('Review Sent');
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
     async function getProductInfo() {
         await getProduct(entryUrl).then(res => {
@@ -66,15 +82,20 @@ export default function Product({ entryUrl }: { entryUrl: string }) {
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <input type="text" placeholder='Enter a review' />
+            <div className='w-full flex flex-col items-center mt-[50px]'>
+                <div className='w-[55%]'>
+                    <h1 className='text-2xl font-bold mb-[10px]'>Send a Review</h1>
+                    <form action="" className='w-full flex items-center justify-between' onSubmit={handleSubmit}>
+                        <input type="number" placeholder='Set Rating' name='rating' className='w-[300px] p-2 border-2' />
+                        <input type="text" placeholder='Enter a review' name='review' className='w-[300px] p-2 border-2' />
+                        <button type='submit' className='w-[100px] p-2 rounded bg-black text-white'>Send</button>
+                    </form>
                 </div>
-                <div className='flex w-[70%]'>
+                <div className='flex w-[70%] flex-col gap-2 mt-[10px]'>
                     {product.rating.map((value, index) => {
                         return (
-                            <div key={index}>
-                                <p>{value.stars.value}</p>
+                            <div key={index} className='p-5 border-2'>
+                                <p className='font-bold'>Rating: {value.stars.value}</p>
                                 <p>{value.review}</p>
                             </div>
                         );
